63623e92a4bd0c420f48672f6c84ece732473d7a
Adept <majumder.sayan02@gmail.com>
2023-03-08 16:31:53 +0530

    Adding .py file to load model

---
+++ VideoCap.py
@@ -0,0 +1,30 @@
+import cv2
+import json
+from deeplearning import yolo_predictions
+cap = cv2.VideoCapture('./TEST/TEST.mov')
+
+output_list = []
+
+while True:
+    ret, frame = cap.read()
+
+    if ret == False:
+        print('Unable to read video')
+        break
+
+    results = yolo_predictions(frame,net)
+    results_list = results.tolist()
+    # Append the predictions to the output_list
+    output_list.append(results_list)
+
+    cv2.namedWindow('YOLO',cv2.WINDOW_KEEPRATIO)
+    cv2.imshow('YOLO',results)
+    if cv2.waitKey(30) == 27 :
+        break
+
+# Write the output_list to a JSON file
+with open('predictions.json', 'w') as f:
+    json.dump(output_list, f)
+
+cv2.destroyAllWindows()
+cap.release()
